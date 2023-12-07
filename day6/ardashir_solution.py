c01ca9d328b9a9177990398710ae979fd93ae94a
ArdashirCHSCSC <152347390+ArdashirCHSCSC@users.noreply.github.com>
2023-12-06 23:50:46 -0600

    Update ardashir_solution.py
    added explanation

--- day6/ardashir_solution.py
+++ day6/ardashir_solution.py
@@ -1,1 +1,4 @@
 ##Empty
+Code uploaded here was faulty and incomplete;
+Further, Mr. Bartucz has requested that Club Leadership refrain from posting solutions beyond Day 4 for the time being.  This code will remain empty until we believe it is acceptable to post solutions.
+Ardashir will submit his solution for Day 6 here soon.
